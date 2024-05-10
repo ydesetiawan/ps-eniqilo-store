25f4c85ee104b1995546ab6b89158c084fa72258
Edy Setiawan <edy.setiwan@mekari.com>
2024-05-11 05:30:08 +0700

    fix test case

--- cmd/api/server/router.go
+++ cmd/api/server/router.go
@@ -18,8 +18,8 @@ func (s *Server) setupRouter() {
 
 	v1.HandleFunc("/product", s.baseHandler.RunActionAuth(s.productHandler.CreateProduct)).Methods("POST")
 	v1.HandleFunc("/product", s.baseHandler.RunActionAuth(s.productHandler.GetProduct)).Methods("GET")
-	v1.HandleFunc("/product/{id:[1-9][0-9]*}", s.baseHandler.RunActionAuth(s.productHandler.UpdateProduct)).Methods("PUT")
-	v1.HandleFunc("/product/{id:[1-9][0-9]*}", s.baseHandler.RunActionAuth(s.productHandler.DeleteProduct)).Methods("DELETE")
+	v1.HandleFunc("/product/{id}", s.baseHandler.RunActionAuth(s.productHandler.UpdateProduct)).Methods("PUT")
+	v1.HandleFunc("/product/{id}", s.baseHandler.RunActionAuth(s.productHandler.DeleteProduct)).Methods("DELETE")
 	v1.HandleFunc("/product/customer", s.baseHandler.RunActionAuth(s.productHandler.SearchSKU)).Methods("GET")
 	v1.HandleFunc("/product/checkout", s.baseHandler.RunActionAuth(s.checkoutHandler.CheckoutProduct)).Methods("POST")
 	v1.HandleFunc("/product/checkout/history", s.baseHandler.RunActionAuth(s.checkoutHandler.GetCheckoutHistory)).Methods("GET")
--- internal/product/dto/product_dto.go
+++ internal/product/dto/product_dto.go
@@ -12,11 +12,11 @@ type ProductReq struct {
 	SKU         string `json:"sku" validate:"required,min=1,max=30"`
 	Category    string `json:"category" validate:"required,oneof=Clothing Accessories Footwear Beverages"`
 	ImageURL    string `json:"imageUrl" validate:"required,url"`
-	Notes       string `json:"notes" validate:"required,min=1,max=200"`
+	Notes       string `json:"notes" validate:"required,min=1,max=201"`
 	Price       int64  `json:"price" validate:"required,min=1"`
 	Stock       int    `json:"stock" validate:"required,min=0,max=100000"`
 	Location    string `json:"location" validate:"required,min=1,max=200"`
-	IsAvailable bool   `json:"isAvailable" validate:"boolean"`
+	IsAvailable bool   `json:"isAvailable" validate:"required,boolean"`
 }
 
 func ValidateProductReq(req ProductReq) error {
