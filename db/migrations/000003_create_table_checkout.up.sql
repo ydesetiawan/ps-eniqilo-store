d5c7cb708028460cf40e02bc99446d029db80621
Risal <risalhidayat88@gmail.com>
2024-05-10 08:18:58 +0700

    fix missing close parenthesis

--- db/migrations/000003_create_table_checkout.up.sql
+++ db/migrations/000003_create_table_checkout.up.sql
@@ -3,7 +3,7 @@ CREATE TABLE checkouts (
     customer_id SERIAL NOT NULL,
     total_price NUMERIC NOT NULL CHECK (total_price >= 1),
     paid NUMERIC NOT NULL CHECK (paid >= 1),
-    change NUMERIC NOT NULL CHECK (change >= 0,
+    change NUMERIC NOT NULL CHECK (change >= 0),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
     deleted_at TIMESTAMP NULL
 );
